1b573fb48c17ddf26e0d72d0ab968f8b50bc9a8a
Angad Sethi <angad.sethi@atlan.com>
2025-05-06 12:58:59 +0530

    Refactor i18n extraction script and update dependencies
    Revised i18n workflow to streamline extraction tasks and added `vue-i18n-extract` dependency for automation. Updated workflow triggers, improved repo checkout logic, and optimized script execution. Adjusted configuration files and enhanced package dependencies to align with new workflow changes.

--- scripts/translationScript.mjs
+++ scripts/translationScript.mjs
@@ -5,6 +5,7 @@ import path from 'path'
 import { glob } from 'glob'
 
 const OPENAPI_API_KEY = process.env.OPENAPI_API_KEY
+const BASE_DIRECTORY = process.env.BASE_DIRECTORY
 
 const OPENAI_API_URL = 'https://api.openai.com/v1/chat/completions'
 
@@ -118,14 +119,14 @@ async function loadLocales(pattern) {
 
 ; (async function () {
     const report = await VueI18NExtract.createI18NReport({
-        vueFiles: './src/**/*.?(js|vue|ts)',
-        languageFiles: './src/locales/**/*.json',
+        vueFiles: `./frontend/src/**/*.?(js|vue|ts)`,
+        languageFiles: `${BASE_DIRECTORY}/src/locales/**/*.json`,
     })
 
     // `vue-i18n-extract` mishandles single quotes within `t` function calls, and double escapes them. As a result
     // they aren't matched to an existing string within the language file, so we manually remove those backslashes
     // and re-filter out entries.
-    const allLocales = await loadLocales('./src/locales/**/*.json')
+    const allLocales = await loadLocales(`${BASE_DIRECTORY}/src/locales/**/*.json`)
 
     const missingKeys = Object.groupBy(
         report.missingKeys
@@ -143,7 +144,7 @@ async function loadLocales(pattern) {
     for (const language in missingKeys) {
         if (language === 'en') {
             fs.writeFileSync(
-                './src/locales/en/default.json',
+                `${BASE_DIRECTORY}/src/locales/en/default.json`,
                 JSON.stringify(
                     {
                         ...allLocales[language],
@@ -165,7 +166,7 @@ async function loadLocales(pattern) {
             )
 
             fs.writeFileSync(
-                `./src/locales/${language}/default.json`,
+                `${BASE_DIRECTORY}/src/locales/${language}/default.json`,
                 JSON.stringify(
                     {
                         ...allLocales[language],
